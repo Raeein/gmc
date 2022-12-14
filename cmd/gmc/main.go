f4fee3e2b67ae52b9e24b43c5f9d382ddefbb54d
Raeein <raeeinbagheri80@gmail.com>
2022-12-14 16:37:41 -0500

    add context to mongodb

--- cmd/gmc/main.go
+++ cmd/gmc/main.go
@@ -7,8 +7,14 @@ import (
 
 func main() {
 	cfg := config.Read()
-	mc := mongodb.New(cfg.Mongo.Username, cfg.Mongo.Password)
-	mc.Log(cfg.Mongo.Database, cfg.Mongo.Collection)
+	mc := mongodb.New(
+		cfg.Mongo.Username,
+		cfg.Mongo.Password,
+		cfg.Mongo.Database,
+		cfg.Mongo.Collection,
+	)
+	mc.Log("Info", "test log entry")
+	mc.Close()
 	//u := gmc.User{"raeein@aol.com", "Raeein"}
 	//email.Send(u, []string{"Java", "Python"}, cfg.Smtp.Host, cfg.Smtp.Port, cfg.Smtp.From, cfg.Smtp.Password)
 	//p := &gmc.User{"jason@me.com", "hi"}
--- mongodb/mongodb.go
+++ mongodb/mongodb.go
@@ -11,25 +11,56 @@ import (
 )
 
 type Logger struct {
-	Client *mongo.Client
-	Ctx    context.Context
+	Client     *mongo.Client
+	DB         string
+	Collection string
 }
 
-func New(username, password string) Logger {
+const timeout = 10 * time.Second
+
+func New(username, password, db, collection string) Logger {
 	serverAPIOptions := options.ServerAPI(options.ServerAPIVersion1)
-	uri := fmt.Sprintf("mongodb+srv://%s:%s@gmc-logs.lp2jrjo.mongodb.net/?retryWrites=true&w=majority", username, password)
+	uri := fmt.Sprintf(
+		"mongodb+srv://%s:%s@gmc-logs.lp2jrjo.mongodb.net/?retryWrites=true&w=majority",
+		username,
+		password,
+	)
 	clientOptions := options.Client().
 		ApplyURI(uri).
 		SetServerAPIOptions(serverAPIOptions)
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
 	client, err := mongo.Connect(ctx, clientOptions)
 	if err != nil {
 		log.Fatal(err)
 	}
-	return Logger{Client: client, Ctx: ctx}
+	return Logger{Client: client, DB: db, Collection: collection}
 }
 
-func (l Logger) Log(db, cl string) {
-	collection := l.Client.Database(db).Collection(cl)
-	collection.InsertOne(l.Ctx, bson.D{{"name", "pi"}, {"value", 3.14159}})
+func (l Logger) Log(title, description string) {
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	location, _ := time.LoadLocation("EST")
+	currentTime := time.Now().In(location).Format("2006-01-02 15:04:05")
+
+	doc := bson.D{
+		{"Title", title},
+		{"Description", description},
+		{"Time", currentTime},
+	}
+	collection := l.Client.Database(l.DB).Collection(l.Collection)
+	res, err := collection.InsertOne(ctx, doc)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	fmt.Println("Inserted new document successfully", res.InsertedID)
+}
+
+func (l Logger) Close() {
+	if err := l.Client.Disconnect(context.TODO()); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Mongo connection closed.")
 }
